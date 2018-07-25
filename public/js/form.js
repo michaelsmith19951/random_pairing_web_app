543779f239dc85517e4a2bf7d730094ad61f52a6
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-25 19:38:11 -0400

    Modified css on first page, added 2 more buttons

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,10 @@
 
-function add_boxes() {
+function addBoxes() {
     var x = document.createElement("INPUT");
     x.setAttribute("type", "text");
-    x.setAttribute("value", "Hello World!");
+    x.setAttribute("value", "Insert Name");
     document.body.appendChild(x);
+}
+function deleteBoxes(boxes) {
+    document.getElementById("addBoxes").removeChild(boxes.parentNode);
 }
