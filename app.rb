e69c270ba34ddaf2dd769d9bab41535b50d43530
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-28 17:53:51 -0400

    Trying to get final page to work correctly

--- app.rb
+++ app.rb
@@ -11,14 +11,20 @@ post '/random_pairs_page1' do
 	results = pairing(addedName)
 	session[:results] = results
 	redirect '/random_pairs_final_page'
-	erb :random_pairs_final_page, locals:{results: results, addedName: addedName}
+	erb :random_pairs_page1, locals:{results: results, addedName: addedName}
 end
 get '/random_pairs_final_page' do
-	# erb :random_pairs_final_page
+	addedName =  session[:addedName]
+	results = session[:results] || ""
+	erb :random_pairs_final_page
 end
 post '/random_pairs_final_page' do
-
+	addedName = params[:addedName] || []
+	results = pairing(addedName)
+	session[:results] = results
+	redirect '/random_pairs_page1'
+	erb :random_pairs_page1
 end
-post '/restart_app' do
+post '/restart' do
 	redirect '/random_pairs_page1'
 end
--- random_pairing_functions.rb
+++ random_pairing_functions.rb
@@ -6,7 +6,7 @@ def pairing(input)
 	else
 		new_array = start.each_slice(2).to_a
 		new_array2 = new_array.last(2)
-		new_array2 = new_array2[0] + new_array2[1]
+		# new_array2 = new_array2[0] + new_array2[1]
 		new_array = new_array[0..-3]
 		new_array.push(new_array2)
 	end
