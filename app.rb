17904ebeff0768d7a7dba139c504b521c379f261
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-30 11:22:22 -0400

    Got results page (mostly) working

--- app.rb
+++ app.rb
@@ -2,27 +2,27 @@ require "sinatra"
 require_relative "random_pairing_functions.rb"
 enable :sessions
 get '/' do
-	addedName =  session[:addedName]
+	name =  session[:name]
 	results = session[:results] || ""
-	erb :random_pairs_page1, locals:{results: results, addedName: addedName}
+	erb :random_pairs_page1, locals:{results: results, name: name}
 end
 post '/random_pairs_page1' do
-	addedName = params[:addedName] || []
-	results = pairing(addedName)
+	puts params
+	name = params[:name] || []
+	results = pairing(name)
 	session[:results] = results
 	redirect '/random_pairs_final_page'
-	erb :random_pairs_page1, locals:{results: results, addedName: addedName}
+	erb :random_pairs_page1
 end
 get '/random_pairs_final_page' do
-	addedName =  session[:addedName]
+	name =  session[:name]
 	results = session[:results] || ""
-	erb :random_pairs_final_page
+	erb :random_pairs_final_page, locals:{results: results, name: name}
 end
 post '/random_pairs_final_page' do
-	addedName = params[:addedName] || []
-	results = pairing(addedName)
+	name = params[:name] || []
+	results = pairing(name)
 	session[:results] = results
-	redirect '/random_pairs_page1'
 	erb :random_pairs_page1
 end
 post '/restart' do
--- public/js/form.js
+++ public/js/form.js
@@ -2,10 +2,11 @@
 function addBoxes() {
     var x = document.createElement("INPUT");
     x.setAttribute("type", "text");
-    x.setAttribute("name", "addedName[]");
+    x.setAttribute("name", "name[]");
     x.setAttribute("placeholder", "");
     var nameMe = document.getElementById("nameMe");
     nameMe.appendChild(x);
+    nameMe.innerHTML = nameMe.innerHTML + "  ";
 }
 function deleteBoxes() {
     var boxesThatIwantDeleted = document.getElementById("nameMe");
